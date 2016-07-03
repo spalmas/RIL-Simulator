6f2bbe6d642ac2c11a334894247e1dbf42f8d514
Sebastian Palmas <sebaspa@gmail.com>
2016-07-03 19:16:02 +0200

    deleting files

--- R/diameter_growth.R
+++
@@ -1,9 +0,0 @@
-# Function to estimate diameter growth from the normal distribution.
-# Does not assign new diameter. For that see diameter_assign
-# this function could also be altered to allow for temporal correlation of growth
-diameter_growth <- function(stand){
-  return (mapply(rnorm,
-                 n = 1, 
-                 mean = stand$DBH.GROWTH.MEAN,
-                 sd = stand$DBH.GROWTH.SD))
-}
